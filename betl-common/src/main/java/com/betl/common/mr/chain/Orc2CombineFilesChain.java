664c0e84bef7ecd5c81cf91099df6315417cc45c
TroubleKid <1768880751@qq.com>
2017-06-05 19:35:03 +0800

    init

---
+++ betl-common/src/main/java/com/betl/common/mr/chain/Orc2CombineFilesChain.java
@@ -0,0 +1,11 @@
+/**
+ * @Email:zhanghelin@geotmt.com
+ * @Author:zhl
+ * @Date:2017年6月5日
+ * @Copyright ZHL All Rights Reserved.
+ */
+package com.betl.common.mr.chain;
+
+public class Orc2CombineFilesChain {
+
+}
