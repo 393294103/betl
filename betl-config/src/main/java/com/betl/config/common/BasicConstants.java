6859215899598a2c750aaa14c9c9f9a14c3a4c0c
TroubleKid <1768880751@qq.com>
2017-06-08 15:48:05 +0800

    betl-config

---
+++ betl-config/src/main/java/com/betl/config/common/BasicConstants.java
@@ -0,0 +1,14 @@
+/**
+ * @Email:zhanghelin@geotmt.com
+ * @Author:zhl
+ * @Date:2017年6月8日
+ * @Copyright ZHL All Rights Reserved.
+ */
+package com.betl.config.common;
+
+public class BasicConstants {
+	
+	
+
+
+}
